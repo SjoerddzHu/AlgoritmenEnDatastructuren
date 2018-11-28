ce2cbb54a51f41d85fa543c49d2d8feeaf2dd5dc
SjoerddzHu <31765239+SjoerddzHu@users.noreply.github.com>
2018-11-28 16:03:20 +0100

    Update PythonWeek2Opdracht1.py

--- week2/PythonWeek2Opdracht1.py
+++ week2/PythonWeek2Opdracht1.py
@@ -1,12 +1,19 @@
+def machtv2(a, n):
+	assert n > 0
+	Counter = 0
+	m = 1
+	while n > 0:
+		if n%2 == 0:
+			a *= a
+			n /= 2
+			Counter += 1 
+		else:
+			m *= a
+			n -= 1
+			Counter += 1 
 
+	print(Counter)
+	return m
 
-
-def machtv3(a,n):
- assert n > 0
- m = 1
- while n > 0:
- if n%2 == 0:
-
- else:
-
- return m
+print(machtv2(8, 4) == 8**4)
+print(machtv2(2, 7) == 2**7)
