e2387e5b7702119f78e86eec982a643a4457df66
SjoerddzHu <31765239+SjoerddzHu@users.noreply.github.com>
2018-12-20 01:08:05 +0100

    Update PythonWeek2Opdracht2.py

--- week2/PythonWeek2Opdracht2.py
+++ week2/PythonWeek2Opdracht2.py
@@ -1,58 +1,32 @@
-class MyStack:
-    def __init__(self, items=[]):
-        super().__init__()
-        self.heap = [0]
-        for i in items:
-            self.heap.append(i)
-            self.__floatUp(len(self.heap) - 1)
+class mystack:
+    def __init__(self):
+        self.data = []
 
-    def push(self, data):
-        self.heap.append(data)
-        self.__floatUp(len(self.heap) - 1)
-
-    def peek(self):
-        if self.heap[1]:
-            return self.heap[1]
-        else:
-            return False
+    def push(self, item):
+        self.data.append(item)
 
     def pop(self):
-        if len(self.heap) > 2:
-            self.__swap(1, len(self.heap) - 1)
-            max = self.heap.pop()
-            self.__bubbleDown(1)
-        elif len(self.heap) == 2:
-            max = self.heap.pop()
-        else:
-            max = False
-        return max
+        if len(self.data) != 0:
+            return self.data.pop(-1)
 
-    def __swap(self, i, j):
-        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+    def peek(self):
+        if len(self.data) != 0:
+            return self.data[-1]
 
-    def __floatUp(self, index):
-        parent = index // 2
-        if index <= 1:
-            return
-        elif self.heap[index] > self.heap[parent]:
-            self.__swap(index, parent)
-            self.__floatUp(parent)
+    def isEmpty(self):
+        if len(self.data) == 0:
+            return True
+        return False
 
-    def __bubbleDown(self, index):
-        left = index * 2
-        right = index * 2 + 1
-        largest = index
-        if len(self.heap) > left and self.heap[largest] < self.heap[left]:
-            largest = left
-        if len(self.heap) > right and self.heap[largest] < self.heap[right]:
-            largest = right
-        if largest != index:
-            self.__swap(index, largest)
-            self.__bubbleDown(largest)
+s = mystack()
+s.push(3)
+s.push(5)
+s.push(7)
+print("peek:", s.peek())
+print("first pop:",  s.pop())
+print("second pop:", s.pop())
+print(s.isEmpty())
+s.pop()
+print(s.isEmpty())
+print(s.data)
 
-
-m = MyStack([95, 3, 21, 110, 6, 87, 99])
-m.push(10)
-print(str(m.heap[0:len(m.heap)]))
-print(str(m.pop()))
-# dit is heapsort waarin ik de gevraagde dingen implementeer
